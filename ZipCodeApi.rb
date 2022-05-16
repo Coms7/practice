0844bded3734d55add5ae7e1003002c1b0dcc4e0
Coms7 <strsim7@gmail.com>
2022-05-16 22:31:52 +0900

    インデント修正、エラー処理メソッド切り出し

--- ZipCodeApi.rb
+++ ZipCodeApi.rb
@@ -5,11 +5,32 @@ require 'sinatra'
 require 'uri'
 require 'net/http'
 
-@client = Mysql2::Client.new(host: "localhost", username: "root", password: '', database: 'ruby_tutorial')
+
+client = Mysql2::Client.new(host: "localhost", username: "root", password: '', database: 'ruby_tutorial')
+
+
+def not_found()
+  data = [
+    {
+      status: 'not found',
+      status_code: 400
+    }
+  ]
+  return
+end
+
+def bad_request()
+  data = [
+    {
+      status: 'bad request',
+      status_code: 400
+    }
+  ]
+end
 
 # 全件取得
 get '/zip_codes' do
-results = @client.query('SELECT id, zip_code, prefecture, city, town_area FROM zip_codes;')
+results = client.query('SELECT id, zip_code, prefecture, city, town_area FROM zip_codes;')
   data =  results.map do |row| 
     hash = {
       id: row ['id'],
@@ -25,23 +46,17 @@ end
 # レコード新規登録
 post '/zip_codes' do
   zip_codes = params[:zip_codes]
-  statement = @client.prepare('INSERT INTO zip_codes(zip_code, prefecture, city, town_area, created_at, updated_at)
+  statement = client.prepare('INSERT INTO zip_codes(zip_code, prefecture, city, town_area, created_at, updated_at)
                                 VALUES (?, ?, ?, ?, current_time, current_time);')
   results = statement.execute(zip_codes["zip_code"], zip_codes["prefecture"], zip_codes["city"], zip_codes["town_area"])
-  if(results.size.zero?)
-    hash = {
-      status: 'bad request',
-      status_code: 400
-    }
-    data.push(hash)
-    data.to_json
+  bad_request().to_json = if results.size.zero? 
   else
-    data = []
-    hash = {
+    data = [
+      {
       status: 'created',
-      status_code: 200
-    }
-    data.push(hash)
+      status_code: 201
+      }
+    ]
   end
     data.to_json
 end
@@ -49,16 +64,9 @@ end
 
 #id指定get
 get '/zip_codes/:id' do
-  statement = @client.prepare('SELECT * FROM zip_codes WHERE id = ?;')
+  statement = client.prepare('SELECT * FROM zip_codes WHERE id = ? ;')
   results = statement.execute(params['id'])
-  if(results.size.zero?)
-    data = []
-    hash = {
-      status: 'not found',
-      status_code: 400
-    }
-    data.push(hash)
-    data.to_json
+  not_found().to_json = if results.size.zero?
   else
     data = results.map do |row|
       hash = {
@@ -70,34 +78,27 @@ get '/zip_codes/:id' do
     }
     end
   end
-  data.to_json
+    data.to_json
 end
 
 #id指定アップデート
 put '/zip_codes/:id' do
-zip_codes = params['zip_code', 'prefecture', 'city', 'town_area']
-statement = @client.prepare('SELECT * FROM zip_codes WHERE id = ? ;' )
-results = statement.execute(params['id'])
-#存在チェック 
-  if(results.size.zeroz?)
-    hash = {
-      status: 'not found',
-      status_code: 400
-    }
-    data.push(hash)
-    data.to_json
+  zip_codes = params['zip_code', 'prefecture', 'city', 'town_area']
+  statement = client.prepare('SELECT * FROM zip_codes WHERE id = ? ;' )
+  results = statement.execute(params['id'])
+  #存在チェック
+  not_found().to_json = if results.size.zeroz?
   else
     #登録処理
-    statement = @client.prepare('UPDATE zip_codes SET zip_code = ?, prefecture = ?, city = ?, town_area = ? WHERE id = ?;')
+    statement = client.prepare('UPDATE zip_codes SET zip_code = ?, prefecture = ?, city = ?, town_area = ? WHERE id = ?;')
     results = statement.execute(zip_codes["zip_code"], zip_codes["prefecture"], zip_codes["city"], zip_codes["town_area"], zip_codes["id"])
     data = []
     hash = {
       status: 'updated',
       status_code: 200
     }
-    data.push(hash)
+    data.push(hash).to_json
   end
-  data.to_json
 end
 
 post '/request_zip_cloud/:zip_code' do
@@ -105,73 +106,51 @@ post '/request_zip_cloud/:zip_code' do
   uri = URI("https://zipcloud.ibsnet.co.jp/api/search?zipcode=#{params['zip_code']}")
   res = Net::HTTP.get_response(uri)
   results = res.body
-  p results
-  if res.is_a?(Net::HTTPSuccess)
-    result_hash.new("bad request", 400)
-    result_hash.to_json
-    data = []
-    hash = {
-      status: 'bad request',
-      status_code: 400
-    }
-    data.push(hash)
-    data.to_json
-  end
+  bad_request().to_json = if res.is_a?(Net::HTTPSuccess)
+  return
 
   if(results['status'] == 500)
-    data = []
-    hash = {
+    data = [
+      {
       status: 'api internal error',
       status_code: 500
     }
-    data.push(hash)
+  ]
     data.to_json
   elsif(results['status'] == 400)
-    data = []
-    hash = {
-      status: 'not found',
-      status_code: 400
-    }
-    data.push(hash)
-    data.to_json
+    not_found().to_json
   else
-#登録処理
+  #登録処理
   @results.map do |row| 
-    hash = {
+    data = [
+      {
       zip_code: row ['zip_code'],
       prefecture: row ['prefecture'],
       city: row['city'],
       town_area: row ['town_area']
     }
-    data.push(hash)
+  ]
   end
     statement = client.prepare('INSERT INTO zip_codes(zip_code, prefecture, city, town_area, created_at, updated_at)
                                 VALUES (?, ?, ?, ?, current_time, current_time);')
-    query = statement.execute(hash["zip_code"], hash["prefecture"], hash["city"], hash["town_area"])
-    data.push(hash)
+    query = statement.execute(data["zip_code"], data["prefecture"], data["city"], data["town_area"])
     data.to_json
   end
 end
 
 #id指定デリート
 delete '/zip_codes/:id' do
-  statement = client.prepare('DELETE FROM zip_codes WHERE id = ?;' )
-  @result = statement.execute(params['id'])
-  if(@result.size == 0)
-    hash = {
-      status: 'not found',
-      status_code: 400
-    }
-    data.push(hash)
-    data.to_json
+statement = client.prepare('DELETE FROM zip_codes WHERE id = ?;' )
+result = statement.execute(params['id'])
+  not_found().to_json = if result.size.zero?
   else
-    data = []
-    hash = {
+    data = [
+      {
       status: 'deleted',
       status_code: 200
     }
-    data.push(hash)
+  ]
   end
-  data.to_json
-end
+    data.to_json
+  end
 end
